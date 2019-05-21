b36bd78a0ae80ad78eef06b9abb1796cd6528908
Luis Rodriguez <luisrdrgz.ing@gmail.com>
2019-05-21 13:33:12 +0200

    navigation logic done

--- src/app/movies-category/movies-category.component.ts
+++ src/app/movies-category/movies-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-category',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesCategoryComponent implements OnInit {
 
-  constructor() { }
+  category: string;
+  validCategories= ['top_rated', 'upcoming', 'popular'];
+
+  constructor(private route: ActivatedRoute,private router: Router) { }
 
   ngOnInit() {
-  }
+    // Accede a los parametros de la uri, pero solo cuando el componente se monta
+    // this.category = this.route.snapshot.params.category;
 
+    this.route.params.subscribe(params => {
+      this.category = params.category.replace('_', ' ');
+      if(this.validCategories.includes(params.category)){
+        //getmovies
+      }else{
+        //redirect
+        this.router.navigate(['/movies/popular'])
+      }
+    })
+  }
 }
